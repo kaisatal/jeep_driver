e86e6200e316e388d5f8bfdcc22b13ff0bffb605
Kaisa Talts <kaisatalts@gmail.com>
2026-03-28 12:45:00 +0200

    Same as previous but minimal setup

--- test/pwm_test.py
+++ test/pwm_test.py
@@ -3,18 +3,12 @@ import time
 
 pins = {
     "red_left": 33,
-    "green_right": 32,
-    "forward": 31,
-    "backward": 29
+    "green_right": 32
 }
 
 GPIO.setmode(GPIO.BOARD)
 
 steering_pins = [pins["red_left"], pins["green_right"]]
-driving_pins = [pins["forward"], pins["backward"]]
-
-for pin in driving_pins:
-    GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)
 
 for pin in steering_pins:
     GPIO.setup(pin, GPIO.OUT)
@@ -26,18 +20,13 @@ for pwm in pwms:
 
 try:
     while True:
-        print("HIGH")
-        for pin in driving_pins:
-            GPIO.output(pin, GPIO.HIGH)
-
+        print("Rising")
         for dc in range(0, 101, 2):
             for pwm in pwms:
                 pwm.ChangeDutyCycle(dc)
             time.sleep(0.2)
 
-        print("LOW")
-        for pin in driving_pins:
-            GPIO.output(pin, GPIO.HIGH)
+        print("Falling")
         for dc in range(100, -1, -2):
             for pwm in pwms:
                 pwm.ChangeDutyCycle(dc)
