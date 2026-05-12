51358ee6dd5fa98447ef2e5d8e3b707e1eaeb02a
kaisatal <kaisatalts@gmail.com>
2026-05-12 18:10:06 +0300

    troubleshooting

--- launch/jeep_driver.launch.py
+++ launch/jeep_driver.launch.py
@@ -5,6 +5,12 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='jeep_driver',
+            executable='drive_logic_node',
+            name='drive_logic_node',
+            output='screen'
+        ),
+        Node(
+            package='jeep_driver',
             executable='jeep_driver_node',
             name='jeep_driver_node',
             output='screen'
@@ -13,12 +19,6 @@ def generate_launch_description():
             package='jeep_driver',
             executable='feedback_node',
             name='feedback_node',
-            output='screen'
-        ),
-        Node(
-            package='jeep_driver',
-            executable='drive_logic_node',
-            name='drive_logic_node',
             output='screen'
         )
     ])
